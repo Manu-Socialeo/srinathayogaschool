778540c8ac6a484f940f3cda41bcc4c87bbcf3e3
SocialeoPVT <socialeopvtltd@gmail.com>
2026-04-06 23:54:49 +0530

    Fix: white screen on Vercel, remove i18n module issue

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,16 +1,15 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import messages from './local/index';
 
 i18n
-  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     lng: 'en',
     fallbackLng: 'en',
     debug: false,
-    resources: messages,
+    resources: {
+      en: { translation: {} }
+    },
     interpolation: {
       escapeValue: false,
     },
